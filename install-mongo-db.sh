94e882a50776359a129e3e53c5708fa630891a2e
Amos Lee <931963081@qq.com>
2025-12-17 15:40:06 +0800

    feat: 🎸 总结项目

---
+++ install-mongo-db.sh
@@ -0,0 +1,22 @@
+# 创建MongoDB仓库文件
+sudo tee /etc/yum.repos.d/mongodb-org-7.0.repo << EOF
+[mongodb-org-7.0]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/8/mongodb-org/7.0/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-7.0.asc
+EOF
+
+# 安装MongoDB
+sudo yum install -y mongodb-org
+
+# 如果报错，可以尝试安装6.0版本
+sudo tee /etc/yum.repos.d/mongodb-org-6.0.repo << EOF
+[mongodb-org-6.0]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/8/mongodb-org/6.0/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-6.0.asc
+EOF
